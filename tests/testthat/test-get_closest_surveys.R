f3ff36b294f2a1ce17817cd45389c22d71847deb
Aleksander Eilertsen <aeilertsen@worldbank.org>
2020-08-27 10:36:28 -0400

    Add unit tests for get_closest_surveys()

---
+++ tests/testthat/test-get_closest_surveys.R
@@ -0,0 +1,14 @@
+# Constants
+df <- data.frame(
+  country_code = 'ABC',
+  survey_year = c(2000, 2001.5, 2008.2, 2015)
+)
+
+test_that('get_closest_surveys() works as expected', {
+  expect_equal(get_closest_surveys(df, 1995)$survey_year, 2000) # Single sided below
+  expect_equal(get_closest_surveys(df, 2018)$survey_year, 2015) # Single sided above
+  expect_equal(get_closest_surveys(df, 2015)$survey_year, 2015) # Reference year = Survey year
+  expect_equal(get_closest_surveys(df, 2005)$survey_year, c(2001.5, 2008.2)) # Both sides
+  expect_equal(get_closest_surveys(df, 2008)$survey_year, c(2001.5, 2008.2)) # Both sides
+  expect_equal(get_closest_surveys(df, 2010)$survey_year, c(2008.2, 2015))   # Both sides
+})
