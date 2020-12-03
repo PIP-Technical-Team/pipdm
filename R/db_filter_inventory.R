22ab6998c0ee5c2334cfa044a492f31c7203a6fc
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-12-03 13:53:17 -0500

    add default names for dsm table

--- R/db_filter_inventory.R
+++ R/db_filter_inventory.R
@@ -1,31 +1,28 @@
 #' filter repository of those surveys that are already in used in dsm_table
 #'
-#' @param raw_inventory dataframe from `paste0(datadir, "_inventory/inventory.fst")`
+#' @param raw_inventory dataframe from `getOption("pip.inventory")`
 #' @param datadir character: PIP-Data directory
 #' @param pipedir character: Pipeline ingestion directory
+#' @param dsm_file character: dsm table file. Dafault `getOption("pip.dsmfile")`
 #'
 #' @return
 #' @export
 #' @import data.table
 #'
 #' @examples
-db_filter_inventory <- function(raw_inventory = NULL,
-                                datadir       = getOption("pip.datadir"),
-                                pipedir       = getOption("pip.pipedir")) {
+db_filter_inventory <-function(raw_inventory = getOption("pip.inventory"),
+                               dsm_file      = getOption("pip.dsmfile"),
+                               datadir       = getOption("pip.datadir"),
+                               pipedir       = getOption("pip.pipedir")) {
 
   # Raw Inventory
-  if (is.null(raw_inventory)) {
-    raw_inventory <-   fst::read_fst(paste0(datadir, "_inventory/inventory.fst"))
-  }
-
-  ri <- data.table::as.data.table(raw_inventory)
+  ri <- fst::read_fst(raw_inventory)
+  setDT(ri)
   ri <- ri[,
            survey_id := gsub("\\.dta", "", filename)
   ][
     toupper(tool) == "PC"
   ]
-
-  dsm_file <- paste0(pipedir, "dsm/deflated_svy_means.fst")
 
   if (fs::file_exists(dsm_file)) {
     # Inventory in Use
--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,9 @@
 pipdm_default_options <- list(
-  pip.dlwdir  = "//wbgfscifs01/GPWG-GMD/Datalib/GMD-DLW/Support/Support_2005_CPI/",
-  pip.datadir = "//w1wbgencifs01/pip/PIP-Data/",
-  pip.pipedir = "//w1wbgencifs01/pip/pip_ingestion_pipeline/"
+  pip.dlwdir    = "//wbgfscifs01/GPWG-GMD/Datalib/GMD-DLW/Support/Support_2005_CPI/",
+  pip.datadir   = "//w1wbgencifs01/pip/PIP-Data/",
+  pip.pipedir   = "//w1wbgencifs01/pip/pip_ingestion_pipeline/",
+  pip.dsmfile   = "//w1wbgencifs01/pip/pip_ingestion_pipeline/dsm/deflated_svy_means.fst",
+  pip.inventory = "//w1wbgencifs01/pip/PIP-Data/_inventory/inventory.fst"
 )
 
 .onLoad <- function(libname, pkgname) {
