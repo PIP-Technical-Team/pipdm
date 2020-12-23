ae923ecca68914c886a801ddfdf499b093d78a25
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-12-23 11:38:06 -0500

    load and clean data separately

---
+++ R/db_load_and_clean.R
@@ -0,0 +1,56 @@
+#' Load and clean welfare data from vector of survey ID
+#'
+#' @param survey_id character: vector of survey ID from
+#' `pipload::pip_find_data(filter_to_pc = TRUE)`
+#' @param maindir character: Main directory path
+#'
+#' @return
+#' @export
+#'
+#' @examples
+db_load_and_clean <- function(survey_id, maindir) {
+
+  #--------- Load data ---------
+  sm <- purrr:::map2(.x = inventory,
+                     .y = maindir,
+                     sf_load_clean)
+
+  sm <- data.table::rbindlist(sm,
+                              use.names = TRUE,
+                              fill = TRUE)
+
+  return(sm)
+}
+
+#' Load one data at a time and clean it
+#'
+#' @param survey_id
+#' @param maindir
+#'
+#' @return
+#'
+#' @examples
+load_clean <- function(survey_id, maindir) {
+
+  sm <- pipload::pip_load_data(survey_id = survey_id,
+                               noisy     = FALSE,
+                               maindir   = maindir)
+
+  if (sm[, unique(gd_type)] %in% c("T01", "T02")) {
+    # Transform to T05
+  }
+
+  if (sm[, unique(source)] != "GROUP") {
+    sm <- suppressMessages(
+      wbpip:::md_clean_data(sm,
+                            welfare = welfare,
+                            weight  = weight
+      )$data
+    )
+  }
+
+  return(sm)
+}
+
+sf_load_clean <- purrr:::possibly(.f = load_clean,
+                                  otherwise = NULL)
