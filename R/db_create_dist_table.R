8bff0da6217c3f08d7523521064162c1528aa766
Aleksander Eilertsen <aeilertsen@worldbank.org>
2021-01-08 10:32:12 -0500

    Add db_create_dist_table

---
+++ R/db_create_dist_table.R
@@ -0,0 +1,26 @@
+#' Create distributional statistics table
+#'
+#' @param dl list: A list with distributional statistics datasets.
+#'
+#' @return data.table
+#' @export
+db_create_dist_table <- function(dl) {
+
+  # Create data frame
+  df <- do.call('rbind', dl) %>%
+    as.data.frame()
+  df[1:5] <- purrr::map_df(
+    df[1:5], function(x) unname(unlist(x)))
+  df$quantiles <- df$quantiles %>% unname()
+  df$survey_id <- row.names(df)
+  row.names(df) <- NULL
+
+  # Convert to data table
+  dt <- df %>%
+    data.table::as.data.table()
+
+  # Order columns
+  data.table::setcolorder(dt, c('survey_id'))
+
+  return(dt)
+}
