0968db2304540980a63fe83f1a26cb102f831e6c
Aleksander Eilertsen <aeilertsen@worldbank.org>
2021-01-18 05:54:21 -0500

    Fix unit tests

--- tests/testthat/test-select_proxy.R
+++ tests/testthat/test-select_proxy.R
@@ -1,62 +1,62 @@
 test_that('select_proxy() works as expected', {
 
   # Non SSA region 1 survey (all PCE values present)
-  df <- data.frame(svy_pce = 100, svy_gdp = 200)
+  df <- data.frame(survey_pce = 100, survey_gdp = 200)
   out <- select_proxy(df, region_code = 'ECA', ref_pce = 105, ref_gdp = 205)
   expect_equal(out, list(value0 = 100, value1 = NULL, req_value = 105))
 
   # Non SSA region 2 surveys (all PCE values present)
-  df <- data.frame(svy_pce = c(100, 110), svy_gdp = c(200, 210))
+  df <- data.frame(survey_pce = c(100, 110), survey_gdp = c(200, 210))
   out <- select_proxy(df, region_code = 'ECA', ref_pce = 105, ref_gdp = 205)
   expect_equal(out, list(value0 = 100, value1 = 110, req_value = 105))
 
   # Non SSA region 1 survey (survey PCE value missing)
-  df <- data.frame(svy_pce = NA, svy_gdp = 200)
+  df <- data.frame(survey_pce = NA, survey_gdp = 200)
   out <- select_proxy(df, region_code = 'ECA', ref_pce = 105, ref_gdp = 205)
   expect_equal(out, list(value0 = 200, value1 = NULL, req_value = 205))
 
   # Non SSA region 1 survey (reference PCE value missing)
-  df <- data.frame(svy_pce = 100, svy_gdp = 200)
+  df <- data.frame(survey_pce = 100, survey_gdp = 200)
   out <- select_proxy(df, region_code = 'ECA', ref_pce = NA, ref_gdp = 205)
   expect_equal(out, list(value0 = 200, value1 = NULL, req_value = 205))
 
   # Non SSA region 2 surveys (one survey PCE value missing)
-  df <- data.frame(svy_pce = c(100, NA), svy_gdp = c(200, 210))
+  df <- data.frame(survey_pce = c(100, NA), survey_gdp = c(200, 210))
   out <- select_proxy(df, region_code = 'ECA', ref_pce = 105, ref_gdp = 205)
   expect_equal(out, list(value0 = 200, value1 = 210, req_value = 205))
 
   # Non SSA region 2 surveys (PCE reference value missing)
-  df <- data.frame(svy_pce = c(100, 110), svy_gdp = c(200, 210))
+  df <- data.frame(survey_pce = c(100, 110), survey_gdp = c(200, 210))
   out <- select_proxy(df, region_code = 'ECA', ref_pce = NA, ref_gdp = 205)
   expect_equal(out, list(value0 = 200, value1 = 210, req_value = 205))
 
   # SSA region 1 survey (all values present)
-  df <- data.frame(svy_pce = 100, svy_gdp = 200)
+  df <- data.frame(survey_pce = 100, survey_gdp = 200)
   out <- select_proxy(df, region_code = 'SSA', ref_pce = 105, ref_gdp = 205)
   expect_equal(out, list(value0 = 200, value1 = NULL, req_value = 205))
 
   # SSA region 2 surveys (all values present)
-  df <- data.frame(svy_pce = c(100, 110), svy_gdp = c(200, 210))
+  df <- data.frame(survey_pce = c(100, 110), survey_gdp = c(200, 210))
   out <- select_proxy(df, region_code = 'SSA', ref_pce = 105, ref_gdp = 205)
   expect_equal(out, list(value0 = 200, value1 = 210, req_value = 205))
 
   # SSA region 1 survey (survey GDP missing)
-  df <- data.frame(svy_pce = 100, svy_gdp = NA)
+  df <- data.frame(survey_pce = 100, survey_gdp = NA)
   out <- select_proxy(df, region_code = 'SSA', ref_pce = 105, ref_gdp = 205)
   expect_equal(out, list(value0 = NA, value1 = NULL, req_value = 205))
 
   # SSA region 2 surveys (one survey GDP value missing)
-  df <- data.frame(svy_pce = c(100, 105), svy_gdp = c(200, NA))
+  df <- data.frame(survey_pce = c(100, 105), survey_gdp = c(200, NA))
   out <- select_proxy(df, region_code = 'SSA', ref_pce = 105, ref_gdp = 205)
   expect_equal(out, list(value0 = 200, value1 = NA_integer_, req_value = 205))
 
   # SSA region 1 survey (reference GDP missing)
-  df <- data.frame(svy_pce = 100, svy_gdp = 200)
+  df <- data.frame(survey_pce = 100, survey_gdp = 200)
   out <- select_proxy(df, region_code = 'SSA', ref_pce = 105, ref_gdp = NA)
   expect_equal(out, list(value0 = 200, value1 = NULL, req_value = NA))
 
   # SSA region 2 surveys (reference GDP value missing)
-  df <- data.frame(svy_pce = c(100, 105), svy_gdp = c(200, 205))
+  df <- data.frame(survey_pce = c(100, 105), survey_gdp = c(200, 205))
   out <- select_proxy(df, region_code = 'SSA', ref_pce = 105, ref_gdp = NA)
   expect_equal(out, list(value0 = 200, value1 = 205, req_value = NA))
 
@@ -64,16 +64,16 @@ test_that('select_proxy() works as expected', {
 
 test_that('check_inputs_select_proxy() catches errors correctly', {
   # Too many rows
-  df <- data.frame(svy_pce = c(100, 105, 110), svy_gdp = c(200, 205, 210))
+  df <- data.frame(survey_pce = c(100, 105, 110), survey_gdp = c(200, 205, 210))
   expect_error(check_inputs_select_proxy(df, 'ECA'))
-  # svy_gdp missing
-  df <- data.frame(svy_pce = c(100, 105))
+  # survey_gdp missing
+  df <- data.frame(survey_pce = c(100, 105))
   expect_error(check_inputs_select_proxy(df, 'ECA'))
-  # svy_pce missing
-  df <- data.frame(svy_gdp = c(100, 105))
+  # survey_pce missing
+  df <- data.frame(survey_gdp = c(100, 105))
   expect_error(check_inputs_select_proxy(df, 'ECA'))
   # Incorrect region codes
-  df <- data.frame(svy_pce = c(100, 105), svy_gdp = c(200, 205))
+  df <- data.frame(survey_pce = c(100, 105), survey_gdp = c(200, 205))
   expect_error(check_inputs_select_proxy(df, 'SST'))
   expect_error(check_inputs_select_proxy(df, 'Sub-Saharan Africa'))
 })
