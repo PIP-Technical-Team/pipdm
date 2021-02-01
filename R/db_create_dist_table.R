991d2734e4b203666e7cd4682b62dd1d2f8997b3
Aleksander Eilertsen <aeilertsen@worldbank.org>
2021-02-01 14:39:23 -0500

    Add deflated median

--- R/db_create_dist_table.R
+++ R/db_create_dist_table.R
@@ -1,15 +1,22 @@
 #' Create distributional statistics table
 #'
+#' Create a table with distributional statistics, include a deflated median.
+#'
 #' @param dl list: A list with distributional statistics datasets.
 #' @param survey_id character: A vector with survey ids.
+#' @inheritParams db_create_ref_year_table
 #'
 #' @return data.table
 #' @export
-db_create_dist_table <- function(dl, survey_id) {
+db_create_dist_table <- function(dl, survey_id, dsm_table) {
 
+  # Checks
   assertthat::assert_that(
     length(dl) == length(survey_id),
     msg = '`dl` and `survey_id` must be of equal lengths.' )
+
+
+  # ---- Covert from list to data table ----
 
   # Create data frame
   dl <- purrr::map(dl, .f = function(x) do.call('rbind', x))
@@ -39,8 +46,45 @@ db_create_dist_table <- function(dl, survey_id) {
   dt <- df %>%
     data.table::as.data.table()
 
+  # ---- Merge with DSM ----
+
+  # Select DSM columns
+  dsm_table <-
+    dsm_table[, .SD, .SDcols =
+                c('survey_id', 'country_code', 'surveyid_year', 'survey_year',
+                  'reporting_year', 'survey_acronym', 'welfare_type',
+                  'cpi', 'ppp')]
+
+  # Merge survey table with CPI (left join)
+  dt <- data.table::merge.data.table(
+    dt, dsm_table, all.x = TRUE,
+    by = 'survey_id')
+
+  # ---- Deflate median ----
+
+  # survey_median_ppp = median / cpi / ppp
+  dt$survey_median_lcu <- dt$median
+  dt$median <- NULL
+  dt$survey_median_ppp <-
+    wbpip::deflate_welfare_mean(
+      welfare_mean = dt$survey_median_lcu, ppp = dt$ppp, cpi = dt$cpi)
+  dt$cpi <- NULL
+  dt$ppp <- NULL
+
+  # --- Finalize table ----
+
+
   # Order columns
-  data.table::setcolorder(dt, c('survey_id', 'pop_data_level'))
+  data.table::setcolorder(
+    dt,
+    c('survey_id', 'country_code', 'survey_acronym',
+      'surveyid_year', 'survey_year', 'reporting_year',
+      'welfare_type', 'pop_data_level', 'survey_median_lcu',
+      'survey_median_ppp'))
+
+
+  # Sort rows
+  data.table::setorder(dt, survey_id)
 
   return(dt)
 }
