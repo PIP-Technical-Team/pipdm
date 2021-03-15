d75a8a9377c2cb5e0405007e3f74c237dfa0bebe
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-03-15 16:32:41 -0400

    force survey_year to be numeric for adjust_aux_values to run

--- R/ts_cache_read_ok.R
+++ R/ts_cache_read_ok.R
@@ -1,18 +1,18 @@
-ts_cache_read_ok <- function() {
-  inv_path  <- "//w1wbgencifs01/pip/PIP-Data/_testing/pip_ingestion_pipeline/pc_data/cache/alt_clean_survey_data/_crr_inventory/crr_inventory.fst"
+#' Check that all cache files load correctly
+#'
+#' @param inv_path character: directory path where cache files are stores
+#'
+#' @return data.table
+#' @export
+#'
+#' @examples
+ts_cache_read_ok <- function(inv_path = "//w1wbgencifs01/pip/PIP-Data/_testing/pip_ingestion_pipeline/pc_data/cache/alt_clean_survey_data/_crr_inventory/crr_inventory.fst"
+                             ) {
 
   inv <- fst::read_fst(inv_path, as.data.table = TRUE)
   # inv <- inv[grepl("ARG.+201[7-9]", filename)]
 
   cch <- inv$cache_file
-  status_fst <- function(x) {
-    fst::read_fst(x)
-    "good"
-  }
-
-
-  safe_fst <- purrr::possibly(.f = status_fst,
-                              otherwise = NA)
 
   df <- purrr::map(.x = cch,
                    .f = safe_fst
@@ -24,6 +24,16 @@ ts_cache_read_ok <- function() {
   if (requireNamespace("pushoverr", quietly = TRUE)) {
     pushoverr::pushover("Finish testing loading")
   }
-
+  return(inv)
 }
+
+
+status_fst <- function(x) {
+  fst::read_fst(x)
+  "good"
+}
+
+
+safe_fst <- purrr::possibly(.f = status_fst,
+                            otherwise = NA)
 
