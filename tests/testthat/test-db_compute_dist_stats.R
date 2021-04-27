0a6674d3b3638b8112fbdd502b2c3a0422c50049
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-04-27 16:19:21 -0400

    update tests

--- tests/testthat/test-db_compute_dist_stats.R
+++ tests/testthat/test-db_compute_dist_stats.R
@@ -34,9 +34,8 @@ test_that("national microdata", {
 
   ld <- get_dist_stata("PHL_2018_FIES_D1_CON_GPWG")
   ll <- ld$ld
-  ln <- ld$ldnames
 
-  expect_equal(names(ll), ln)
+  expect_equal(names(ll), "national")
 
   for(i in seq_along(ll)) {
     expect_equal(names(ll[[i]]), dnames)
@@ -49,7 +48,7 @@ test_that("urban/rural microdata", {
 
   ld <- get_dist_stata("IDN_2000_SUSENAS_D2_CON_GPWG")
   ll <- ld$ld
-  ln <- ld$ldnames
+  ln <- c("national", "rural", "urban")
 
   expect_equal(names(ll), ln)
   for(i in seq_along(ll)) {
@@ -59,7 +58,6 @@ test_that("urban/rural microdata", {
 
   ld <- get_dist_stata("IND_2004_NSS-SCH1_D2_CON_GPWG")
   ll <- ld$ld
-  ln <- ld$ldnames
 
   expect_equal(names(ll), ln)
   for(i in seq_along(ll)) {
@@ -73,20 +71,20 @@ test_that("national Group Data", {
 
   ld <- get_dist_stata("ARE_2019_HIES_D1_INC_GROUP")
   ll <- ld$ld
-  ln <- ld$ldnames
+  ln <- c("national")
 
   expect_equal(names(ll), ln)
   expect_equal(names(ll[[1]]), dnames)
 
 })
 
-test_that("Urban/Group Group Data", {
+test_that("Urban/Rural Group Data", {
 
   ld <- get_dist_stata("IDN_1990_SUSENAS_D2_CON_GROUP")
   ll <- ld$ld
-  ln <- ld$ldnames
+  ln <- c("national", "rural", "urban")
 
-  expect_equal(names(ll), c("national", ln))
+  expect_equal(names(ll), ln)
 
   for(i in seq_along(ll)) {
     expect_equal(names(ll[[i]]), dnames)
@@ -99,7 +97,7 @@ test_that("Imputed Data", {
 
   ld <- get_dist_stata("SOM_2017_SHFS-W2_D1_CON_GPWG")
   ll <- ld$ld
-  ln <- ld$ldnames
+  ln <- c("national")
 
   expect_equal(names(ll), ln)
   expect_equal(names(ll[[1]]), dnames)
