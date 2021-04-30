0eaaec63646be361140c50daa85d6ed2ae7b3702
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-04-30 14:58:44 -0400

    document

--- R/pip_class.R
+++ R/pip_class.R
@@ -1,13 +1,31 @@
+#' Add pipmd class
+#'
+#' @param x data frame
+#'
+#' @return data frame with new class pipmd
+#' @export
 as_pipmd <- function(x) {
   class(x) <- pipmd_class
   x
 }
 
+#' Add pipgd class
+#'
+#' @param x data frame
+#'
+#' @return data frame with new class pipgd
+#' @export
 as_pipgd <- function(x) {
   class(x) <- pipmgd_class
   x
 }
 
+#' Add pipid class
+#'
+#' @param x data frame
+#'
+#' @return data frame with new class pipid
+#' @export
 as_pipid <- function(x) {
 
   class(x) <- pipid_class
