306a88d77e57030b5593c276c5922c069e5d5e8d
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-08-30 16:21:26 -0400

    add globals and root_dir

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,23 @@
+
+# defined values
+root_dir  <-  Sys.getenv("PIP_ROOT_DIR")
+# root_dir  = Sys.getenv("PIP_ROOT_DIRfff")
+
+if (root_dir != "") {
+  # globals
+  gls <- pipload::pip_create_globals(root_dir)
+} else {
+  delayedAssign("gls", pipload::pip_create_globals(root_dir))
+}
+
+# if you don't the official value in `Sys.getenv("PIP_ROOT_DIR")` you can
+# provide the object `root_dir  <- "<you directory>"` before executing the first
+# fucntion pipaux. In this way, object `gls`, which is a promise, will be
+# created using with you `root_dir`. Otherwise, you can especify the complete
+# directory path for each function.
+
+
+
 pip_pipedir <- "//w1wbgencifs01/pip/pip_ingestion_pipeline/"
 pipuax_default_options <- list(
   pip.dlwdir      = "//wbgfscifs01/GPWG-GMD/Datalib/GMD-DLW/Support/Support_2005_CPI/",
