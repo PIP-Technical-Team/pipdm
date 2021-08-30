adde230fd5a0c4aee3c011153157c5c9942f75d9
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-08-30 16:17:52 -0400

    document

---
+++ R/pipdm.R
@@ -0,0 +1,13 @@
+#' pipdm: A package for computating the notorious bar statistic
+#'
+#' The pipdm package provides three categories of important functions:
+#' pipdm, bar and baz.
+#'
+#' @section pipdm functions:
+#' The pipdm functions ...
+#'
+#' @docType package
+#' @name pipdm
+#' @importFrom pipload pip_create_globals
+NULL
+#> NULL
