7961d7297cb8589c3c24f98f33058131b3e16234
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-08-31 10:27:52 -0400

    New globals

--- R/zzz.R
+++ R/zzz.R
@@ -17,20 +17,16 @@ if (root_dir != "") {
 # directory path for each function.
 
 
-
-pip_pipedir <- "//w1wbgencifs01/pip/pip_ingestion_pipeline/"
-pipuax_default_options <- list(
-  pip.dlwdir      = "//wbgfscifs01/GPWG-GMD/Datalib/GMD-DLW/Support/Support_2005_CPI/",
-  pip.maindir     = "//w1wbgencifs01/pip/PIP-Data_QA/",
-  pip.pipedir     = pip_pipedir,
-  pip.cachedir.pc = paste0(pip_pipedir, "pc_data/cache/clean_survey_data/"),
-  pip.cachedir.tb = paste0(pip_pipedir, "tb_data/cache/clean_survey_data/")
+pipdm_default_options <- list(
+  pip.dm = ""
 )
 
 .onLoad <- function(libname, pkgname) {
   op <- options()
-  toset <- !(names(pipuax_default_options) %in% names(op))
-  if (any(toset)) options(pipuax_default_options[toset])
+  toset <- !(names(pipdm_default_options) %in% names(op))
+  if (any(toset)) options(pipdm_default_options[toset])
+
+  assign('gls', gls, envir = topenv())
 
   invisible()
 }
