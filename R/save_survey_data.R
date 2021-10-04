cc11ab9f150f5daa3bd391de963344099ff8f47b
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-10-04 16:12:11 -0400

    add `save` and `load` arguments to save_survey_data

--- R/save_survey_data.R
+++ R/save_survey_data.R
@@ -4,18 +4,24 @@
 #'
 #' @param dt Data frame from cache to be saved
 #' @param cols character: Vector with columns to save. If NULL all columns are
-#'  saved.
+#'   saved.
 #' @param output_dir character: Output folder.
 #' @param future_plan character: `future` plan to use.
 #' @param compress numeric: Compression level used in `fst::write_fst()`.
+#' @param save logical: if TRUE save data file. Default is TRUE
+#' @param load logical: if TRUE loads data. Defatul is FALSE and returns file
+#'   directory path
 #' @param cache_filename character: Vector with new names for microdata.
+#'
 #' @export
 save_survey_data <- function(dt,
-                             cols = NULL,
+                             cols        = NULL,
                              output_dir,
                              cache_filename,
                              future_plan = c('sequential', 'multisession', 'callr'),
-                             compress) {
+                             compress,
+                             save        = TRUE,
+                             load        = FALSE) {
 
   # Select columns
   if (!is.null(cols)) {
@@ -34,27 +40,47 @@ save_survey_data <- function(dt,
 
   svy_out_path <- paste(output_dir, cache_filename, sep = "/")
 
-  fst_status <-
-    tryCatch(
-      expr = {
-        # Your code...
-        fst::write_fst(x        = dt,
-                       path     = svy_out_path,
-                       compress = compress)
-        "passed"
-      }, # end of expr section
+  if (file.exists(svy_out_path)) {
+    odt <- fst::read_fst(svy_out_path)
 
-      error = function(e) {
-        "error"
-      }, # end of error section
+    if (identical(odt, dt)) {
+      cli::cli_alert_info("{cache_filename} has not changed")
+      save <- FALSE
+    } else {
+      cli::cli_alert_warning("{cache_filename} has changed")
+    }
 
-      warning = function(w) {
-        "warning"
-      }
-    ) # End of trycatch
+  }
 
-  attr(svy_out_path, "fst_status") <- fst_status
+  if (save) {
+    fst_status <-
+      tryCatch(
+        expr = {
+          # Your code...
+          fst::write_fst(x        = dt,
+                         path     = svy_out_path,
+                         compress = compress)
+          "passed"
+        }, # end of expr section
 
-  return(svy_out_path)
+        error = function(e) {
+          "error"
+        }, # end of error section
+
+        warning = function(w) {
+          "warning"
+        }
+      ) # End of trycatch
+    attr(svy_out_path, "fst_status") <- fst_status
+  }  else {
+    attr(svy_out_path, "fst_status") <- "Not saved"
+  }
+
+  if (load) {
+    return(dt)
+  } else {
+    return(svy_out_path)
+  }
+
 }
 
