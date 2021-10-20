93093b4003518ca79beda66e903b39a9a0c6ac2e
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-10-19 23:43:25 -0400

    fix bugs

--- R/delete_old_file.R
+++ R/delete_old_file.R
@@ -1,9 +1,9 @@
 #' Delete cache files that are no longer in use
 #'
-#' @param cache_ids character:vector with  Cache_ids. default from
-#'   `db_create_pipeline_inventory()`
 #' @param target_dir character: directory where the cache files are stored. If
 #'   could be a patch, like `gls$OUT_SVY_DIR_PC` from
+#' @param cache_ids character:vector with  Cache_ids. default from
+#'   `db_create_pipeline_inventory()`
 #'   `pipload::add_gls_to_env()` or it could be one of the two shortcuts, "in"
 #'   or "out". The former refers to `gls$CACHE_SVY_DIR_PC`. the latter refers to
 #'   `gls$OUT_SVY_DIR_PC`.
@@ -17,10 +17,10 @@
 #'
 #' @examples
 #' delete_old_output_file(delete = FALSE)
-delete_old_file <- function(cache_ids = NULL,
-                                   target_dir = "in",
-                                   verbose    = getOption("pipdm.verbose"),
-                                   delete = NULL) {
+delete_old_file <- function(target_dir = "in",
+                            cache_ids  = NULL,
+                            verbose    = getOption("pipdm.verbose"),
+                            delete     = NULL) {
 
   if (is.null(cache_ids)) {
     dt <- db_create_pipeline_inventory()
@@ -35,11 +35,12 @@ delete_old_file <- function(cache_ids = NULL,
   }
 
 
-  dir_files <- list.files(path = target_dir,
-                          full.names = TRUE)
+  dir_files <- list.files(path       = target_dir,
+                          pattern    = "^[^_]", # whatever does not start with _
+                          full.names = TRUE,
+                          recursive  = FALSE)
 
   file_names <- gsub("(//.+/)|([\\.].+$)", "", dir_files)
-
 
   names_delete <- file_names[!(file_names %in% cache_ids)]
 
